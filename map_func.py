194746606d7e3d2895171cf5de101dac112da351
dawid <dawidciupka@gmail.com>
2021-09-21 13:08:29 -0700

    update map

---
+++ map_func.py
@@ -0,0 +1,12 @@
+txt = ['Funkcja lambda to funkcje anonimowe',
+	'Funkcje anonimowe nie maja nazwy',
+	'Funkcje w Pythonie sa obiektami.']
+
+mark = map(lambda s : (True,s) if 'anonimowe' in s else (False, s), txt)
+print(list(mark))
+print(10*'---')
+
+Nie = 'nie'
+Tak = 'tak'
+nie = map(lambda word : (Nie, word) if 'nie' in  word else (Tak, word), txt)
+print(list(nie))
---
+++ opn_file.py
@@ -0,0 +1,1 @@
+print([line.strip() for line in open('opn_file.py')])
