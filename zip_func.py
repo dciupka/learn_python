339c3a3bf6ab1a3d679514c763ea890dc43c32b0
dawid <dawidciupka@gmail.com>
2021-09-29 11:48:50 -0700

    update zip func

--- zip_func.py
+++ zip_func.py
@@ -14,3 +14,23 @@ list_1, list_2 = zip(*ziped)
 
 print(list(list_1))
 print(list_2)
+
+print(10 * "****")
+
+
+names = ['dawid', 'mariola', 'mirek', 'marek', 'andrzej',]
+surnames = ['ciupka', 'klimiuk', 'sowa', 'kryspinski', 'zigfido',]
+
+
+name_surname = list(zip(names,surnames))
+
+
+# Wynik
+print(name_surname)
+
+
+names_1, surnames_1 = zip(*name_surname)
+
+print(names_1)
+print(surnames_1)
+
