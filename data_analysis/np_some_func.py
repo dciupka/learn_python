f274d6de273c85947d92e9b4e845a8408d37b501
dawid <dawidciupka@gmail.com>
2021-10-05 08:32:00 -0700

    update lotto with numpy

--- data_analysis/np_some_func.py
+++ data_analysis/np_some_func.py
@@ -1,11 +1,10 @@
 import numpy as np
 from random import randint
-array_of_numbers = np.array([x for x in range(1,50)])
-
-print(array_of_numbers)
+ars = np.array([x for x in range(1,50)])
 
 index = randint(0,49)
 
-for x in array_of_numbers:
-	print(x)
+#drawed_numbers= np.array([ars[index],ars[index]])
 
+drawed_numbers= np.array([[ars[randint(0,49-x)] for x in range(1,7)] for los in range(0,1000000)])
+print(drawed_numbers)
