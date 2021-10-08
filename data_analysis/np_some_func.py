24ddfe9072bdef7418feb01dc2fd6274b97fc0af
dawid <dawidciupka@gmail.com>
2021-10-08 11:29:12 -0700

    func lotto numpy working

--- data_analysis/np_some_func.py
+++ data_analysis/np_some_func.py
@@ -1,10 +1,13 @@
 import numpy as np
-from random import randint
-ars = np.array([x for x in range(1,50)])
+from random import randint, choice
+ars = [x for x in range(1,50)]
 
 index = randint(0,49)
 
+
+
 #drawed_numbers= np.array([ars[index],ars[index]])
 
-drawed_numbers= np.array([[ars[randint(0,49-x)] for x in range(1,7)] for los in range(0,1000000)])
-print(drawed_numbers)
+drawed_numbers= np.array([[ars.pop(randint(0,48-x)) for x in range(1,7)] for los in range(0,10000)])
+for i in drawed_numbers:
+	print(set(i))
