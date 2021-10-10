4248bfd2b10cb8acbe14b09ea4eb6c83d94e4621
dawid <dawidciupka@gmail.com>
2021-10-10 11:39:57 -0700

    some func do zrobienia

--- data_analysis/np_some_func.py
+++ data_analysis/np_some_func.py
@@ -1,13 +1,9 @@
 import numpy as np
 from random import randint, choice
 ars = [x for x in range(1,50)]
+z=49
+print(ars)
 
-index = randint(0,49)
 
+# lotto funkcja z np
 
-
-#drawed_numbers= np.array([ars[index],ars[index]])
-
-drawed_numbers= np.array([[ars.pop(randint(0,48-x)) for x in range(1,7)] for los in range(0,10000)])
-for i in drawed_numbers:
-	print(set(i))
