95a10fa79da8afaf700e999ab3ebe4a2037d9057
dawid <dawidciupka@gmail.com>
2021-10-12 13:18:56 -0700

    work on lotto np func

--- data_analysis/np_some_func.py
+++ data_analysis/np_some_func.py
@@ -1,9 +1,12 @@
 import numpy as np
 from random import randint, choice
-ars = [x for x in range(1,50)]
-z=49
-print(ars)
-
-
+liczby_do_wylosowania = [x for x in range(1,50)]
+zakres = 49
 # lotto funkcja z np
-
+pusta_lista =[]
+for losowanie in range(1,7):
+	index=randint(1,zakres)
+	zakres-=1
+	liczba=liczby_do_wylosowania.pop(index)
+	pusta_lista.append(liczba)
+	print(set(pusta_lista), len(set(pusta_lista)))
